bb0508d0ffc1b82ea14350a4a95c85784e9f8756
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-01-04 08:00:20 +0530

    Create arr1.java

---
+++ Scaler/Array/arr1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+
+		//int[] marks = new int[10];
+		int[] m = {1,2,3,4,5}; 
+		System.out.println(m[0]+" "+m[4]);
+
+		float[] arr = new float[5];
+		System.out.println(arr[2]);
+
+		String[] names = {"Azar","T4TEQ","ZOHO","Apple"};
+		System.out.println(names[0]);
+	}
+}
