8bc6621b0dba4f4b9ccb7128f2f61d7f693c9953
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-01-04 07:57:29 +0530

    Create constant.java

---
+++ Scaler/Basic/constant.java
@@ -0,0 +1,9 @@
+class Main
+{
+	public static void main(String args[])
+	{
+		final int a=10;//like const in c programming
+		//a=20; we can't assign any value  to a
+		System.out.println(a);
+	}
+}
