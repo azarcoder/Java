11460ed6ad4ca3dba8d9a57fc941adf44d71a947
Azarudeen <76832585+azarcoder@users.noreply.github.com>
2023-02-23 16:20:07 +0530

    Create goto.java

---
+++ Scaler/pattern/goto.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int n = sc.nextInt(),k=0;
+		int l = s.length();
+		label:
+			while(true)
+			{
+				for(int i = 0 ; i < l ; i++)
+				{
+					for(int j = l-1 ; j >= i ; j--)
+							System.out.print(" ");
+					for(int j = 0 ; j <= i ; j++)
+					    System.out.print(s.charAt(j));
+					System.out.print("  ");
+					for(int j = i ; j >= 0 ; j--)
+					    System.out.print(s.charAt(j));
+					System.out.println();
+				}
+				k++;
+				if(k==n) break;
+		}
+
+	}
+}
